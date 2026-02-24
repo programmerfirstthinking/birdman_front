66807859c49ea0532aae3bf86e5a59af5616a4f4
chako-mountain <gonzaburouduanyewufu@gmail.com>
2026-02-24 21:03:51 +0900

    フロントからトピック作成成功

--- app/srccode/topic/page.tsx
+++ app/srccode/topic/page.tsx
@@ -1,6 +1,5 @@
 "use client"; // Next.js 13 appディレクトリの場合
-
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 // import { GoogleAuthProvider } from "firebase/auth";
 import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, reload } from "firebase/auth";
@@ -26,6 +25,11 @@ const app = initializeApp(firebaseConfig);
 
 export default function Main() {
 
+  // const [fetchData, setFetchData] = useState({});
+  let topicdata ;
+
+  const [data, setData] = useState<Record<string, any> | null>(null);
+
   async function fetchtopicdata() {
     await fetch("http://localhost:8080/topics", {
       method: "GET",
@@ -33,16 +37,26 @@ export default function Main() {
         "Content-Type": "application/json",
       },
     }).then((response) => response.json())
+    //   console.log("トピックデータの取得に成功"), 
+    //   console.log(response)
+    
+    // });
     .then((data) => {
       console.log("トピックデータの取得に成功");
-      console.log(data);  
+      console.log(data);
+      // topicdata = data;
+      // console.log(topicdata);
+      // setFetchData(data);
+      setData(data);
     });
   }
 
+  useEffect(() => {
   fetchtopicdata();
+}, []);
 
 
-  
+
   const [topicName, setTopicName] = useState("");
   const [topicContent, setTopicContent] = useState("");
 
@@ -57,7 +71,7 @@ export default function Main() {
 
       // GlobalIdToken = idToken;
 
-      await fetch("http://localhost:8080/topiccoment", {
+      await fetch("http://localhost:8080/topiccontent", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -66,10 +80,13 @@ export default function Main() {
     });
   }
 
+ 
+
   return (
     <div>
       <h2>トピックの作成はこちら</h2>
         {/* <form  className="signup" method="POST"> */}
+        <div></div>
         <form
             className="signup"
             onSubmit={async (e) => {
@@ -93,6 +110,23 @@ export default function Main() {
           />
         <button type="submit">トピックの作成</button>
       </form>
+
+      <div>
+      {/* <button onClick={fetchData}>取得</button> */}
+
+      {data && (
+        <div>
+          {Object.entries(data).map(([key, value]) => (
+            <div key={key}>
+              <strong>{key}:</strong>{" "}
+              {typeof value === "object"
+                ? JSON.stringify(value)
+                : String(value)}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
     </div>
   )
 }
