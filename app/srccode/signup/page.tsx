6ce1c75c56e9b6b1667e04aae6d480257c3f2b47
chako-mountain <gonzaburouduanyewufu@gmail.com>
2026-02-24 16:08:33 +0900

    topicの内容、名前、現在のトークンをバックに送信

--- app/srccode/signup/page.tsx
+++ app/srccode/signup/page.tsx
@@ -1,19 +1,9 @@
-// import Link from "next/link"
-
-// export default function Main() {
-//   return (
-//     <Link href="http://localhost:8080/test">
-//       <button>Aboutへ</button>
-//     </Link>
-//   )
-// }
-
 "use client"; // Next.js 13 appディレクトリの場合
 
 import { useState } from "react";
 // import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 // import { GoogleAuthProvider } from "firebase/auth";
-import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, reload } from "firebase/auth";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
@@ -21,11 +11,7 @@ import { getAnalytics } from "firebase/analytics";
 import { sign } from "crypto";
 // import { useRouter } from 'next/router';
 import { useRouter } from 'next/navigation';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCC3c0UgIJ9P9_BUXBLCw1GPPiHFwHvTrk",
   authDomain: "share-info-project.firebaseapp.com",
@@ -38,15 +24,32 @@ const firebaseConfig = {
 
 const provider = new GoogleAuthProvider();
 
+let GlobalIdToken = "";
+
+
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 
+// function reloadPage() {
+//   const router = useRouter();
+
+//   if (GlobalIdToken!="") {
+//     router.push('/srccode/topic');
+//   }
+// }
+
+// reloadPage();
+
 export default function Signup() {
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const router = useRouter();
+
+
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.name === "mail") {
@@ -118,49 +121,14 @@ export default function Signup() {
         const auth = getAuth();
         signInWithPopup(auth, provider)
           // .then(async (result) => {
-          //   const user = result.user;
-          //   console.log("Googleサインイン成功:", user);
-          //   alert("Googleでのサインインに成功しました");
-          //   // fetch("")
-          //   const idToken = await user.getIdToken();
 
-          //   const fetchData = async () => {
-          //     try {
-          //       const res = await fetch('http://127.0.0.1:8080/login', {
-          //         method: 'POST',
-          //         headers: {
-          //           'Content-Type': 'application/json',
-          //         },
-          //         // body: JSON.stringify({ userId: user.uid }),
-
-          //         body: JSON.stringify({ idToken }), // IDトークンを送信
-          //       });
-
-          //       if (!res.ok) {
-          //         throw new Error(`サーバーエラー: ${res.status}`);
-          //       }
-
-
-
-          //       // const data = await res.json();
-          //       // console.log("サーバー応答:", data);
-
-          //     } catch (error) {
-          //       console.error("Fetchエラー:", error);
-          //     }
-          //   };
-
-          //   fetchData();
-
-          //   router.push('/srccode/topic');
-
-          // })
           .then(async (result) => {
             const user = result.user;
 
             alert("Googleでのサインインに成功しました");
 
             const idToken = await user.getIdToken();
+            GlobalIdToken = idToken;
 
             router.push('/srccode/topic');
 
@@ -170,8 +138,12 @@ export default function Signup() {
                 "Content-Type": "application/json",
               },
               body: JSON.stringify({ idToken }),
+
+              
               
             });
+
+            console.log("グローバル:", GlobalIdToken);
           })
           .catch((error) => {
             console.error(error);
--- app/srccode/topic/page.tsx
+++ app/srccode/topic/page.tsx
@@ -1,10 +1,54 @@
+"use client"
+
 import Link from "next/link"
+import { useState } from "react"
+import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, reload } from "firebase/auth";
 
 export default function Main() {
+  
+  const [topicName, setTopicName] = useState("");
+  const [topicContent, setTopicContent] = useState("");
+
+  async function sendinfo() {
+
+      // 現在のユーザーのトークンを取得している
+      //　ログインしているかをチェック
+      const auth = getAuth();
+      const user = auth.currentUser;
+
+      const idToken = await user?.getIdToken();
+
+      // GlobalIdToken = idToken;
+
+      await fetch("http://localhost:8080/topiccoment", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name: topicName, content: topicContent,token:idToken }),
+    });
+  }
+
   return (
     <div>
       <h2>トピックの作成はこちら</h2>
+        <form  className="signup" method="POST">
+          <input
+            type="text"
+            name="topicname"
+            placeholder="トピック名"
+            value={topicName}
+            onChange={(e)=>setTopicName(e.target.value)}
+          />
+          <input
+            type="text"
+            name="topiccontent"
+            placeholder="内容"
+            value={topicContent}
+            onChange={(e)=>setTopicContent(e.target.value)}
+          />
+        <button onClick={sendinfo}>トピックの作成</button>
+      </form>
     </div>
   )
 }
-
