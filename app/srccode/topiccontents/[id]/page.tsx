b0c9211ab2a31f562729e3daf4b55114af5d8d24
chako-mountain <gonzaburouduanyewufu@gmail.com>
2026-02-25 19:05:12 +0900

    トピックに対してコメントを残せた

--- app/srccode/topiccontents/[id]/page.tsx
+++ app/srccode/topiccontents/[id]/page.tsx
@@ -3,6 +3,19 @@
 import { ParamValue } from "next/dist/server/request/params";
 import { useParams } from "next/navigation";
 import { useState, useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import { getAuth,onAuthStateChanged, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, reload } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCC3c0UgIJ9P9_BUXBLCw1GPPiHFwHvTrk",
+  authDomain: "share-info-project.firebaseapp.com",
+  projectId: "share-info-project",
+  storageBucket: "share-info-project.firebasestorage.app",
+  messagingSenderId: "10017220780",
+  appId: "1:10017220780:web:4820d384929f2d84735709",
+  measurementId: "G-42VYEZ51GF"
+};
+
 
 export default function Page() {
   const params = useParams();
@@ -22,32 +35,40 @@ export default function Page() {
     };
   
   const [results, setResults] = useState<Topic>();
+  const [topic_comment, setTopicComment] = useState("");
 
+  type Comment = {
+  ID: number;
+  UserID: number;
+  TopicID: number;
+  Content: string;
+  CreatedAt: string;
+};
 
+const [comments, setComments] = useState<Comment[]>([]);
+
+  const app = initializeApp(firebaseConfig);
 
   function GetTopicdata(id: ParamValue) {
-    fetch("http://localhost:8080/topics/" + id, {
+    fetch(`http://localhost:8080/topic_comment_only/${id}`, {
       method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
     })
       .then((response) => {
         if (!response.ok) {
-          throw new Error(`HTTPエラー: ${response.status} ${response.statusText}`);
+          throw new Error("取得失敗");
         }
         return response.json();
       })
       .then((data) => {
-        console.log("トピックデータの取得に成功");
-        console.log(data.topics);
-        setResults(data.topics); // ←ここ重要
+        console.log("取得成功:", data.comments);
+        setComments(data.comments);
       })
       .catch((error) => {
         console.error(error);
-        // alert(`トピックデータの取得エラー: ${error.message}`);
       });
-   }
+  }
+
+
 
     useEffect(() => {
       if (id) {
@@ -55,15 +76,97 @@ export default function Page() {
       }
     }, [id]);
 
+  async function sendinfo() {
+
+      // const params = useParams();
+      // const id = params.id;
+
+      const auth = getAuth();
+      const user = auth.currentUser;
+
+      if (!user) {
+        console.error("ユーザーがログインしていません");
+        return;
+      }
+
+      const idToken = await user.getIdToken();
+      console.log("現在のユーザーのIDトークン:", idToken);
+
+
+      const response = await fetch("http://localhost:8080/topic_comment", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ topicId:id, topic_comment:topic_comment, token: idToken }),
+      });
+
+
+      if (!response.ok) {
+        console.error(`HTTPエラー: ${response.status} ${response.statusText}`);
+        return;
+      }
+
+      const data = await response.json();
+
+      console.log("これがレスポンスです");
+      console.log(data);
+      console.log(data.comments);
+
+      setComments(data.comments);
+
+
+
+
+  }
+
 
 
   return (
     <div>
       <h1>URLの最後</h1>
-      <p>{id}</p>
-      <div>{results?.ID}</div>
-      <div>{results?.TopicName}</div>
-      <div>{results?.Content}</div>
+      <h2>トピックの内容</h2>
+      <div>
+        <p>{id}</p>
+        <div>{results?.ID}</div>
+        <div>{results?.TopicName}</div>
+        <div>{results?.Content}</div>
+
+      </div>
+
+      <div>
+
+
+        <h2>コメント一覧</h2>
+        {comments?.map((comment) => (
+          <div key={comment.ID}>
+            <p>{comment.Content}</p>
+            <p>ユーザーID: {comment.UserID}</p>
+            <p>作成日時: {comment.CreatedAt}</p>
+          </div>
+        ))}
+      </div>
+      <div>
+        <h3>トピックに対するコメントをしましょう</h3>
+        <form
+            className="signup"
+            onSubmit={async (e) => {
+              e.preventDefault();
+              await sendinfo();
+            }}
+          >
+          <input
+            type="text"
+            name="topiccontent"
+            placeholder="内容"
+            value={topic_comment}
+            onChange={(e)=>setTopicComment(e.target.value)}
+          />
+          <button type="submit">コメントを投稿</button>
+        </form>
+        
+      </div>
+
     </div>
   );
 }
