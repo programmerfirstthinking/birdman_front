ba547d0f1134604638f9a836be01f97a3ca4cfd2
chako-mountain <gonzaburouduanyewufu@gmail.com>
2026-03-10 22:54:03 +0900

    firebaseconfigを変更した

--- src/app/firebaseconfig/firebase.ts
+++ src/app/firebaseconfig/firebase.ts
@@ -12,14 +12,25 @@
 
 
 
-// src/lib/firebase.ts
+// // src/lib/firebase.ts
+
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyCC3c0UgIJ9P9_BUXBLCw1GPPiHFwHvTrk",
+//   authDomain: "share-info-project.firebaseapp.com",
+//   projectId: "share-info-project",
+//   storageBucket: "share-info-project.firebasestorage.app",
+//   messagingSenderId: "10017220780",
+//   appId: "1:10017220780:web:4820d384929f2d84735709",
+//   measurementId: "G-42VYEZ51GF",
+// };
+
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyCC3c0UgIJ9P9_BUXBLCw1GPPiHFwHvTrk",
-  authDomain: "share-info-project.firebaseapp.com",
-  projectId: "share-info-project",
-  storageBucket: "share-info-project.firebasestorage.app",
-  messagingSenderId: "10017220780",
-  appId: "1:10017220780:web:4820d384929f2d84735709",
-  measurementId: "G-42VYEZ51GF",
+  apiKey: "AIzaSyCUvqt8c4KYiUlj62_1EEqRymWD1nFBygg",
+  authDomain: "birdmanweb-2d750.firebaseapp.com",
+  projectId: "birdmanweb-2d750",
+  storageBucket: "birdmanweb-2d750.firebasestorage.app",
+  messagingSenderId: "628017139067",
+  appId: "1:628017139067:web:26589463eee87d4b1301de",
+  measurementId: "G-GSSBEQBLYR"
 };
